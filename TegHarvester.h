6332e0db99fbbdc82c26589aee71a695833fb4da
Kristian Korsgaard <kristianrkorsgaard@gmail.com>
2018-02-13 15:25:36 +0100

    Added documentation

--- TegHarvester.h
+++ TegHarvester.h
@@ -18,20 +18,31 @@ public:
     byte storageVoltagePinA=6 /* Analog */,
     double aref=3.3 /* Analog Reference Voltage */);
 
+  // Run to initialize digital ports
   void begin();
 
+  // Enable sample on TegHarvester
   void enableSample();
   void disableSample();
   void toggleSample();
 
+  // Read Voltage sample from TegHarvester if "enableSample" is set,
+  // otherwise return sample from memory
   double getSample() const;
+  // Set sample in memory
   void setSample(double sample);
 
+  // Set analog reference value to calculate voltage sampleState_
+  // Use in conjunction with analogReference() in Arduino IDE
   void setAnalogReference(double aref = 3.3);
+  // Return stored analog reference
   void getAnalogReference() const;
 
+  // Set keep_alive on TegHarvester, to keep Core alive
   void keepAlive(bool alive = true);
 
+  // Set offset for analog read, to calibrate measurements (samples)
+  // The analog read return a value between 0-1023, to which this value is added
   void setOffset(char offset = -4);
   char getOffset() const;
 
